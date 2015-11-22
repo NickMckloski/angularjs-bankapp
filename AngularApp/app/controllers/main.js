51c4a8fa92976fb68482b2d730438ba199f48dca
Nick Mckloski <Mckloski.Nick@gmail.com>
2015-11-22 16:03:20 -0500

    entry form validation

--- AngularApp/app/controllers/main.js
+++ AngularApp/app/controllers/main.js
@@ -1,14 +1,30 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('app')
-        .controller('Main', main);
+    angular.module('app').controller('Main', main);
 
     function main() {
         var controller = this;
-        controller.entries = [{ name: 'Rent', date: '11/22/2015', type: 'Expense', cost: 500 },
-                              { name: 'Food', date: '11/22/2015', type: 'Expense', cost: 200 }]
+        //controller.entries = [{ name: 'Rent', date: '11/22/2015', type: 'Expense', cost: 500 },
+        //                      { name: 'Food', date: '11/22/2015', type: 'Expense', cost: 200 }];
+        controller.entries = [];
+
+        //function to add a new entry
+        controller.addEntry = function () {
+            controller.entries.push({ name: controller.formEntryName, date: controller.formEntryDate, type: controller.formEntryType, cost: controller.formEntryCost });
+        }
+
+        //function to submit the entry form      
+        controller.submitForm = function (isValid) {
+
+            // check to make sure the form is valid
+            if (isValid) {
+                alert('our form is valid');
+            } else {
+                alert('not valid');
+            }
+
+        };
     }
 
 })();
