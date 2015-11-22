feb1078d9a094de690d5023b9e5462fa79dae92a
Nick Mckloski <Mckloski.Nick@gmail.com>
2015-11-22 14:53:33 -0500

    init commit

---
+++ AngularApp/app/controllers/main.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main() {
+        var controller = this;
+        controller.entries = [{ name: 'Rent', date: '11/22/2015', type: 'Expense', cost: 500 },
+                              { name: 'Food', date: '11/22/2015', type: 'Expense', cost: 200 }]
+    }
+
+})();
