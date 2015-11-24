079752ad986f4a67707d6d2c9aba3cfd0b116c8b
Nick Mckloski <Mckloski.Nick@gmail.com>
2015-11-24 01:13:55 -0500

    interest calculator started

---
+++ AngularApp/app/controllers/Interest.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('Interest', interest);
+
+    function interest() {
+        var controller = this;
+
+        controller.interest = "interest calc";
+    }
+
+})();
