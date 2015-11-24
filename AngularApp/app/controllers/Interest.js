e696028549307b8905644ad03f5838a6d4522875
Nick Mckloski <Mckloski.Nick@gmail.com>
2015-11-24 15:12:58 -0500

    compound interest done

--- AngularApp/app/controllers/Interest.js
+++ AngularApp/app/controllers/Interest.js
@@ -6,7 +6,32 @@
     function interest() {
         var controller = this;
 
-        controller.interest = "interest calc";
+        //show output
+        controller.compOuput = false;
+        
+        //function to show compoun output
+        controller.showCompOutput = function () {
+            controller.compOuput = true;
+        }
+
+        //function to calculate compound interest
+        controller.calCompound = function () {
+            var starting = controller.compStarting;
+            var addition = controller.compAddition;
+            var timesAnnually = controller.compTimes;
+            var years = controller.compYears;
+            var rate = controller.compRate/100;
+
+            //unit test
+            //var a = starting * Math.pow((1 + rate / timesAnnually), (timesAnnually * years));
+            //var b = Math.pow(1 + (rate / timesAnnually), (timesAnnually * years)) - 1;
+            //var c = (rate / timesAnnually);
+            //var d = (addition * ( b / c))
+            //var out = a + d;
+            //console.log(a + " " + b + " " + c + " " + d + " " + out);
+
+            return (starting * Math.pow((1 + rate / timesAnnually), (timesAnnually * years))) + (addition * ((Math.pow(1 + (rate / timesAnnually), (timesAnnually * years)) - 1) / (rate / timesAnnually)));
+        }
     }
 
 })();
